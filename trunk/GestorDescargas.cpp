57ff4ffda3cc17cdeb0f03a1e7eab4105a15e930
gygounet@gmail.com <gygounet@gmail.com@d77367b4-a41a-a8ec-1c07-1d8232531fd3>
2011-10-02 16:18:44 +0000

    Descargando archivos en carpeta descargas+pidpadre

--- trunk/GestorDescargas.cpp
+++ trunk/GestorDescargas.cpp
@@ -27,25 +27,25 @@ int GestorDescargas::iniciarRecepcion() {
 	string pathLockEscritura = intToString(pidEnvia) + ".lockEscritura";
 	LockFile lockEscritura((char*)pathLockEscritura.c_str());
 
-	int pidDestino;
-	char id[4];
-	canal.leer(id,4);
-	pidDestino = atoi(id);
 	int bytesLeidos = canal.leer(buffer,BUFFSIZE);
-	buffer[bytesLeidos] = '\0';
 	lockEscritura.liberarLock();
+	buffer[bytesLeidos] = '\0'; //controlar esto
+	string linea(buffer);
+	int pidDestino = atoi(linea.substr(0,linea.find("|",0)).c_str());
+	string path = linea.substr(linea.find("|",0) + 1, linea.length());
 
 	cout << "Receptor " << pidEnvia << ": lei el dato [" << buffer << "] del pipe" << pidDestino<< endl;
 
 	int pid = fork ();
 	if (pid == 0) {
-		int resultado = enviar(pidEnvia,pidDestino, buffer);
+		int resultado = enviar(pidEnvia,pidDestino, (char*)path.c_str());
 		canal.cerrar();
+		lockEscritura.cerrar();
 		exit(resultado);
 	}
-
 //}
 	cout << "Receptor " << pidEnvia << ": fin del proceso" << endl;
+	lockEscritura.cerrar();
 	canal.cerrar();
 	return 0;
 }
@@ -63,11 +63,12 @@ int GestorDescargas::enviar(int pidEnvia, int pidDestino, char* buffer) {
 	char linea[BUFFSIZE];
 	while(!archivo.eof()) {
 		archivo.getline(linea,BUFFSIZE);
-		cout << "linea " << linea << endl;
 		lock.tomarLock();
 		canal.escribir(linea,BUFFSIZE);
 		cout << "ya escribi " << linea << endl;
 	}
+	archivo.close();
+	lock.cerrar();
 	canal.cerrar();
 	return 0;
 }
@@ -80,23 +81,37 @@ int GestorDescargas::descargar(string path,int pidEnvia)
 	Fifo canal((char*)intToString(pidEnvia).c_str());
 
 	lockEscritura.tomarLock();
-	string id = intToString(pid);
-	canal.escribir(id.c_str(), id.length());
-	canal.escribir(path.c_str(),path.length());
+	string pidPath = intToString(pid) + "|" + path;
+	canal.escribir(pidPath.c_str(), pidPath.length());
+
+	canal.cerrar();
+	lockEscritura.cerrar();
 
 	string pathFifoDescarga = intToString(pidEnvia) + "_" + intToString(pid);
 	string pathLockDescargaEscritura = pathFifoDescarga + ".lockEscritura";
 	LockFile lockDescargaEscritura((char*)pathLockDescargaEscritura.c_str());
 	Fifo canalDescarga((char*)pathFifoDescarga.c_str());
 
-	char idDescarga[4];
-	canalDescarga.leer(idDescarga,4);
-	pid = atoi(idDescarga);
-	cout<<"descargandooooooooo" << pid <<endl;
-	lockDescargaEscritura.liberarLock();
-
-	//cerrar!!!
-	cout<<"descargandooooooooo" << pid <<endl;
+	ofstream archivo;
+	string directorio = "descargas" + intToString(getppid()) + "/";
+	//TODO ver si no conviene hacer un unico directorio con todas las descargas, se crea en otro lado
+	//y se usa el pid del proceso padre.
+	int estado = mkdir(directorio.c_str(), 0700);// TODO control de errores
+	cout << "Directorio [ " << directorio <<" ] creado para realizar la descarga." << endl; //debug
+	string pathTotal = directorio+path;
+	archivo.open(pathTotal.c_str(),ofstream::out);//open del archivo path,  lee una linea en binario
+	char descarga[BUFFSIZE];
+	while(canalDescarga.leer(descarga,BUFFSIZE) != 0) {
+		lockDescargaEscritura.liberarLock();
+		cout <<"descargandooooooooo" << descarga << endl;
+		archivo << descarga << endl; //TODO endl esta mal parche!!!
+		archivo.flush();
+	}
+	lockDescargaEscritura.liberarLock(); //ver si esta bien
+	archivo.close();
+	canalDescarga.cerrar();
+	lockDescargaEscritura.cerrar();
+	cout<<"DESCARGADO" <<endl;
 	return 0;
 }
 
