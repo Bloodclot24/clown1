c373b5364c4510ec26b81298c2970f0ce2275c58
karenroberts16 <karenroberts16@d77367b4-a41a-a8ec-1c07-1d8232531fd3>
2011-10-07 14:13:46 +0000

    clase Vista

---
+++ trunk/Vista.h
@@ -0,0 +1,43 @@
+#ifndef VISTA_H_
+#define VISTA_H_
+
+#include<iostream>
+
+#include "Usuario.h"
+
+using namespace std;
+
+class Vista {
+public:
+	Vista();
+	virtual ~Vista();
+
+	static void mostrarBienvenida();
+
+	static void mostrarMenu();
+
+	static void mostrarMenuCompartir();
+
+	static void mostrarArchivos(vector<Usuario>& usuarios, Usuario usuario);
+
+	static void mostrarUsuario(Usuario usuario);
+
+	static string pedirString();
+
+	static char pedirChar();
+
+	static int pedirInt(int inferior, int superior);
+
+	static void mostrarMensaje(string mensaje);
+
+	static void mostrarMensajeInicial(string mensaje);
+
+	static void mostrarMensajeFinal(string mensaje);
+
+	static void debug(string mensaje, int entero);
+
+	static void debug(string mensaje);
+
+};
+
+#endif /* VISTA_H_ */
