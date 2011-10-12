d938cd54bfc58f7c378841c7c810ed725b090601
karenroberts16 <karenroberts16@d77367b4-a41a-a8ec-1c07-1d8232531fd3>
2011-10-12 18:54:08 +0000

    clase ConcuShare

---
+++ trunk/ConcuShare.h
@@ -0,0 +1,48 @@
+#ifndef CONCUSHARE_H_
+#define CONCUSHARE_H_
+
+//#include <string.h>
+//#include <iostream>
+//#include <stdlib.h>
+//#include <sys/wait.h>
+//#include <list>
+#include <getopt.h>
+
+#include "GestorUsuarios.h"
+#include "GestorDescargas.h"
+#include "Debug.h"
+#include "Vista.h"
+
+#define HIJO		0
+#define PADRE		1
+#define SALIR		-1
+
+using namespace std;
+
+class ConcuShare {
+private:
+	GestorDescargas gestorDescargas;
+	GestorUsuarios gestorUsuarios;
+	Usuario usuario;
+	list<int> hijos;
+
+	int compartirArchivos();
+
+	int descargarArchivo(Usuario usuarioOrigen);
+
+	int buscarArchivos();
+
+	int ejecutarMenu();
+
+	void crearDirectorioDescargas(string nombre);
+
+	int parsearLineaDeComandos(int argc, char** argv);
+
+public:
+	ConcuShare();
+	virtual ~ConcuShare();
+
+	int iniciar(int argc, char** argv);
+};
+
+#endif /* CONCUSHARE_H_ */
